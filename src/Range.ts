0696653e0cd4c631889623cfd92e5302482d3d69
Leo Schurrer <l.schurrer@oth-aw.de>
2022-07-22 23:47:23 +0200

    fix range has, get, set traps
    added tests for named traps

--- src/Range.ts
+++ src/Range.ts
@@ -1,25 +1,32 @@
 import SmartRange from "./SmartRange";
 
 const handler: ProxyHandler<SmartRange> = {
-    has: (target, prop) => {
+    has(target, prop) {
         if (typeof prop !== "symbol") {
             const key = +prop;
-            if (key !== NaN) return target.includes(key);
+            if (!isNaN(key)) return target.includes(key);
         }
         return Reflect.has(target, prop);
     },
-    get: (target, prop, receiver) => {
-        if (typeof prop === "symbol")
-            return Reflect.get(target, prop, receiver);
-        return target.at(+prop);
+    set(target, prop, value) {
+        return Reflect.set(target, prop, value);
     },
-    ownKeys(target) {
-        console.warn("range: ownKeys is not yet implemented");
-        return [
-            ...Array.from(target.keys(), (k) => k.toString()),
-            ...Reflect.ownKeys(target),
-        ];
+    get(target, prop) {
+        if (prop === Symbol.iterator)
+            return target[Symbol.iterator].bind(target);
+        if (typeof prop !== "symbol") {
+            const key = +prop;
+            if (!isNaN(key)) return target.at(key);
+        }
+        return Reflect.get(target, prop);
     },
+    // ownKeys(target) {
+    //     console.warn("range: ownKeys is not yet implemented");
+    //     return [
+    //         ...Array.from(target.keys(), (k) => k.toString()),
+    //         ...Reflect.ownKeys(target),
+    //     ];
+    // },
 };
 
 /**
@@ -31,19 +38,19 @@ const handler: ProxyHandler<SmartRange> = {
  * ```ts
  * const r = range(1, 10, 1);
  * // default behaviour `key in object` -> key is property of object
- * // overritten by Proxy `value in SmartRange` -> value is included in SmartRange
+ * // overwritten for numbers by Proxy `value in SmartRange` -> value is included in SmartRange
  * 5 in range // true
  * -5 in range // false
  * // lazy computation of indexed values
  * range[4] // 5
  * range[9] // undefined
- * // negative indexing possible -> this.lenght + -index
+ * // negative indexing possible -> this.length + -index
  * range[-1] // 9
  * ```
  * @param start start of the range can be larger than end with negative step
  * @param end end of the range
  * @param step step between values in the range - can be negative defaults to 1 or -1 if end < start
- * @returns {Proxy<SmartRange>} proxy to a SmartRange
+ * @returns {SmartRange} SmartRange wrapped by Proxy
  */
 const range = (start: number, end: number, step?: number) =>
     new Proxy(new SmartRange(start, end, step), handler);
--- tests/Range.test.ts
+++ tests/Range.test.ts
@@ -2,11 +2,93 @@ import { describe, test, expect } from "vitest";
 import range from "../src";
 
 describe("range", () => {
+    test("get trap", () => {
+        let r = range(0, 10);
+        expect(r.start).toBe(0);
+        expect(r["end"]).toBe(10);
+        expect(r.length).toBe(10);
+        expect(r[0]).toBe(0);
+        expect(r[1]).toBe(1);
+        expect(r[9]).toBe(9);
+        expect(r[10]).toBe(undefined);
+        expect(r[-1]).toBe(9);
+        expect(r[-2]).toBe(8);
+        expect(r[-9]).toBe(1);
+        expect(r[-10]).toBe(0);
+        expect(r[-11]).toBe(undefined);
+
+        r = range(0, -10, -2);
+        expect(r.length).toBe(-5);
+        expect(r[0]).toBe(0);
+        expect(r[1]).toBe(-2);
+        expect(r[4]).toBe(-8);
+        expect(r[5]).toBe(undefined);
+        expect(r[-1]).toBe(-8);
+        expect(r[-2]).toBe(-6);
+        expect(r[-4]).toBe(-2);
+        expect(r[-5]).toBe(0);
+        expect(r[-6]).toBe(undefined);
+    });
+
+    test("set/ get trap", () => {
+        const r = range(-14, -8);
+        const r2 = range(20, 30, 5);
+
+        expect(r.start).toBe(-14);
+        expect(r.end).toBe(-8);
+        expect(r.step).toBe(1);
+        expect(r.length).toBe(6);
+        expect([...r]).toEqual([-14, -13, -12, -11, -10, -9]);
+
+        r.start = 2;
+        expect(r.start).toBe(2);
+        expect(r.end).toBe(-8);
+        expect(r.length).toBe(0);
+        expect([...r]).toEqual([]);
+
+        r.step = -3;
+        expect(r.step).toBe(-3);
+        expect([...r]).toEqual([2, -1, -4, -7]);
+        expect(r.length).toBe(-4);
+
+        r.end = 10;
+        expect(r.end).toBe(10);
+        expect(r.length).toBe(0);
+        expect([...r]).toEqual([]);
+
+        r.step = 0;
+        expect(r.step).toBe(1);
+        expect(r.length).toBe(8);
+        expect([...r]).toEqual([2, 3, 4, 5, 6, 7, 8, 9]);
+
+        expect(r2.start).toBe(20);
+        expect(r2.end).toBe(30);
+        expect(r2.step).toBe(5);
+        expect(r2.length).toBe(2);
+        expect([...r2]).toEqual([20, 25]);
+    });
+
     test("has trap", () => {
-        const r = range(-5, 10);
-        expect(5 in r).toBe(true);
-        expect(-1 in r).toBe(true);
-        expect(10 in r).toBe(false);
-        expect(-10 in r).toBe(false);
+        const r = range(150, 160, 2);
+        expect(150 in r).toBe(true);
+        expect(151 in r).toBe(false);
+        expect(152 in r).toBe(true);
+        expect(0 in r).toBe(false);
+        expect(2 in r).toBe(false);
+        r.start = -4;
+        expect(-4 in r).toBe(true);
+        expect(-3 in r).toBe(false);
+        expect(-2 in r).toBe(true);
+        expect("start" in r).toBe(true);
+        expect("end" in r).toBe(true);
+        expect("step" in r).toBe(true);
+        expect("length" in r).toBe(true);
+        expect("next" in r).toBe(true);
+        expect("return" in r).toBe(true);
+        expect("includes" in r).toBe(true);
+        expect("at" in r).toBe(true);
+        expect("forEach" in r).toBe(true);
+        expect("isValid" in r).toBe(false);
+        expect("defaultStep" in r).toBe(false);
     });
 });
